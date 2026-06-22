37472d383247fbad17314241146a041727254d7a
LeAnhKiet <leanhk75@gmail.com>
2026-06-22 18:13:13 +0700

    Solve leaky-relu

---
+++ study-plans/cuda-basics/leaky-relu/leaky-relu.cu
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+
+__global__ void leaky_relu_kernel(const float* input, float* output, float alpha, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i<N) {
+        float x = input[i];
+        output[i] = fmaxf(alpha *x,x);
+    }
+}
+
+extern "C" void solve(const float* input, float* output, float alpha, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    leaky_relu_kernel<<<blocks, threads>>>(input, output, alpha, N);
+    cudaDeviceSynchronize();
+}
